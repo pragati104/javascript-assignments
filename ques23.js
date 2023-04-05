22de50375a7fb2774647d6dc6a9884769d9ef3f6
Pragati kumari <79071905+pragati104@users.noreply.github.com>
2023-04-06 01:47:27 +0530

    Update ques23.js

--- ques23.js
+++ ques23.js
@@ -1,14 +1,16 @@
-num = 15
-let count = 0
-for(let i = 2; i<5;i++){
-    if(num%i !== 0){
-         count = 1
-    }else{
-        count = 0
-    }
+//Write a program to check that the number given by the user is a prime number or not.
+
+num = 15;
+let count = 0;
+for (let i = 1; i <= num; i++) {
+  if (num % i === 0) {
+    count++;
+  }
 }
 
-if(count === 1){
-    console.log("prime number")
-}else{
-    console.log("not prime number")}
+if (count === 2) {
+  console.log("prime number");
+} else {
+  console.log("not prime number");
+}
+count = 0;
