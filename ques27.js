12f225cdbaca0aad46bd6d5430cc9f0cfb5ac7a8
Pragati kumari <79071905+pragati104@users.noreply.github.com>
2023-04-06 01:48:33 +0530

    Update ques27.js

--- ques27.js
+++ ques27.js
@@ -1,6 +1,13 @@
 // 27. Write a program to print all the prime number between 0 to 100 (0 and 100 included).
-
-let num=100;
-for(){
-
+let count = 0;
+for (let i = 0; i <= 100; i++) {
+  for (let j = 0; j <= i; j++) {
+    if (i % j === 0) {
+      count++;
+    }
+  }
+  if (count === 2) {
+    console.log(i);
+  }
+  count = 0;
 }
