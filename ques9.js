90cd98d9607be599399aca61fd85285e1a9c147e
Pragati kumari <79071905+pragati104@users.noreply.github.com>
2023-04-05 21:03:58 +0530

    Update ques9.js

--- ques9.js
+++ ques9.js
@@ -6,7 +6,6 @@
 // let life = true;
 // let death = true;
 
-// 21. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list
 
  
 
